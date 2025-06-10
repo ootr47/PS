4aa86a15b763a6e2164fe2737b1909937089c0ce
ootr47 <ootr45@gmail.com>
2025-06-10 23:21:27 +0900

    B_G5_1052

---
+++ src/main/java/day02_0610/1052.java
@@ -0,0 +1,24 @@
+package day02_0610;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        for(int j=0; j<k; j++){
+            for(int i=1; i<=20000000; i*=2) {
+                if(i >= n){
+                    if(j==k-1) System.out.println(i-n);
+                    else n = n - i/2;
+                    break;
+                }
+            }
+        }
+        
+    }
+}
