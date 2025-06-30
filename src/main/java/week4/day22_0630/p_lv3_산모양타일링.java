8c48cbf7c249d592c8947011387368b12681364d
ootr47 <ootr45@gmail.com>
2025-07-01 01:40:07 +0900

    P_LV3_산 모양 타일링

---
+++ src/main/java/week4/day22_0630/p_lv3_산모양타일링.java
@@ -0,0 +1,19 @@
+class Solution {
+    int[] arr;
+    public int solution(int n, int[] tops) {
+        arr = new int[200002];
+        return test(n*2+1, tops);
+    }
+    
+    public int test(int n, int[] tops) {
+        if(n<2) return 1;
+        if(arr[n]!=0) return arr[n];
+        
+        arr[n] = test(n-1, tops) + test(n-2,tops);
+        if (n%2 == 0 && tops[n/2-1] == 1) arr[n] += test(n-1, tops);   
+        
+        arr[n] %= 10007;
+        
+        return arr[n];
+    }
+}
