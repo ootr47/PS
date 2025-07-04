133358163a2e3d3fd80505add83413ccd31ac6d0
ootr47 <ootr45@gmail.com>
2025-07-04 18:57:00 +0900

    B_G4_1107

---
+++ 1107.java
@@ -0,0 +1,66 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main {
+
+    static String result = "";
+    static int minSub;
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int target = sc.nextInt();
+        int n = sc.nextInt();
+        int[] errorButton = new int[n];
+
+        int[] buttons = new int[10 - n];
+        for (int i = 0; i < n; i++) {
+            errorButton[i] = sc.nextInt();
+        }
+
+        minSub = Math.abs(target - 100);
+
+        if (n == 10) {
+            System.out.println(minSub);
+            return;
+        }
+
+        int t = 0;
+        Arrays.sort(errorButton);
+        for (int i = 0; i < 10; i++) {
+            if (t < n && errorButton[t] == i) {
+                t++;
+                continue;
+            }
+            buttons[i - t] = i;
+        }
+
+        search(buttons, target, new StringBuilder(""));
+
+        System.out.println(minSub);
+    }
+
+    private static void search(int[] buttons, int target, StringBuilder sb) {
+
+        if (sb.length() > 0) {
+            int currentChannel = Integer.parseInt(sb.toString());
+            int pressCount = sb.length() + Math.abs(target - currentChannel);
+
+            if (minSub > pressCount) {
+                minSub = pressCount;
+                result = sb.toString();
+            }
+        }
+        
+        if (sb.length() > 6) {
+            return;
+        }
+
+        for (int i : buttons) {
+            search(buttons, target, sb.append(i));
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
--- src/main/java/week4/day24_0702/1034.java
+++ src/main/java/week4/day24_0702/1034.java
@@ -1,6 +1,4 @@
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 class Main {
@@ -8,69 +6,43 @@ class Main {
     static int n;
     static int m;
     static int k;
-
     static int result = 0;
 
     public static void main(String[] args) throws IOException {
-        
+
         Scanner sc = new Scanner(System.in);
 
         n = sc.nextInt();
         m = sc.nextInt();
 
-        int[][] arr = new int[n][m];
-        int[] countArr = new int[n];
-
-        for(int i=0; i<n; i++) {
-            String input = sc.next();
-
-            for(int j=0; j<m; j++) {
-                arr[i][j] = input.charAt(j) - '0';
-            }
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next();
         }
+
         k = sc.nextInt();
 
-        for(int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             int count = 0;
-            for(int j=0; j<m; j++) {
-                if(arr[i][j] == 0) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i].charAt(j) == '0') {
                     count++;
                 }
             }
-            countArr[i] = count;
-            count = 0;
-        }
 
-        test(arr, countArr, 0, 0);
+            if (count <= k && (k - count) % 2 == 0) {
+                int sum = 0;
+                for (int l = 0; l < n; l++) {
+                    if (arr[i].equals(arr[l])) {
+                        sum++;
+                    }
+                }
+                if (result < sum) {
+                    result = sum;
+                }
+            }
+        }
 
         System.out.println(result);
-    }
-
-    private static void test(int[][] arr, int[] countArr, int switchedCount, int cur) {
-
-        if(cur == m) {
-            int sum = 0;
-            for(int i=0; i<n; i++) {
-                if(countArr[i] == 0) sum++;
-            }
-            
-            if(switchedCount > k) return;  
-            if(result < sum && (k-switchedCount) % 2 == 0) result = sum;
-            return;
-        }
-
-        test(arr, countArr, switchedCount, cur+1);
-
-        for(int i=0; i<n; i++) {
-            if(arr[i][cur] == 0) countArr[i]--;
-            else countArr[i]++;
-        }
-        
-        test(arr, countArr, switchedCount+1, cur+1);
-        
-        for(int i=0; i<n; i++) {
-            if(arr[i][cur] == 0) countArr[i]++;
-            else countArr[i]--;
-        }
     }
 }
